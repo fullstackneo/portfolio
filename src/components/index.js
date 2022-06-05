5cbda61c922f1208499ce49b0f6bbd8bc60b7381
Neo <fullstackneo@gmail.com>
2022-06-04 23:27:41 -0600

    add components structure

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Header from './Header';
+import Footer from './Footer';
+import Navigation from './Navigation';
+import Project from './Project';
+import Contact from './Contact';
+
+export { Header, Footer, Navigation, Project, Contact };
