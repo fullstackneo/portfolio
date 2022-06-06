0052e068a3ee590a7ecefce82d09712e9caeffbf
Neo <fullstackneo@gmail.com>
2022-06-06 00:30:54 -0600

    add Contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { validateEmail } from '../utils/helpers';
 
 function Contact() {
-  return <section>Contact</section>;
-}
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+  const [errorMessage, setErrorMessage] = useState('');
+  const { name, email, message } = formState;
 
+  function handleChange(e) {
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+
+      // isValid conditional statement
+      if (!isValid) {
+        setErrorMessage('Your email is invalid.');
+      }
+    } else {
+      if (!e.target.value.trim().length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage('');
+      }
+    }
+
+    // async function
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+  }
+
+  console.log('errorMessage', errorMessage);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formState);
+  }
+
+  return (
+    <section>
+      <h1>Contact me</h1>
+      <form id="contact-form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            name="name"
+            defaultValue={name}
+            onBlur={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input
+            type="email"
+            name="email"
+            defaultValue={email}
+            onBlur={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea
+            name="message"
+            rows="5"
+            defaultValue={message}
+            onBlur={handleChange}
+          />
+          {errorMessage && (
+            <div>
+              <p className="error-text">{errorMessage}</p>
+            </div>
+          )}
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+  );
+}
 export default Contact;
